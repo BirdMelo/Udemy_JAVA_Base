c37ebc634e5224689f0b59f8b716637231424397
BirdMelo <jpmn2013.terra.com.br@gmail.com>
2025-11-01 11:44:04 -0300

    Exercicio08 feito

---
+++ src/Section04_ifFunction/Exercicios/Exercicio08.java
@@ -0,0 +1,26 @@
+package Section04_ifFunction.Exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio08 {
+    public static void main(String[] args) {
+        Scanner write = new Scanner(System.in);
+        double nota01, nota02, avg;
+
+        System.out.print("Digite a 1° nota: ");
+        nota01 = write.nextDouble();
+        System.out.print("Digite a 2° nota: ");
+        nota02 = write.nextDouble();
+
+        avg = (nota01 + nota02) /2;
+
+        if( avg >= 7.0) {
+            System.out.printf("Média: %.1f | Aprovado", avg);
+        } else if( avg < 5.0) {
+            System.out.printf("Média: %.1f | Reprovado", avg);
+        } else {
+            System.out.printf("Média: %.1f | Recuperação", avg);
+        }
+
+    }
+}
