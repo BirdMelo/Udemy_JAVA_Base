86750397fc7c6f91e466d44f10ba66977eca18d9
BirdMelo <jpmn2013.terra.com.br@gmail.com>
2025-11-08 10:43:52 -0300

    Exercicio01 de switch feito

---
+++ src/Section05_Switch/Exercicios/Exercicio01.java
@@ -0,0 +1,37 @@
+package Section05_Switch.Exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio01 {
+    public static void main(String[] args) {
+        Scanner write = new Scanner(System.in);
+        System.out.print("Digite um número de 1 a 7: ");
+        int day = write.nextInt();
+        write.nextLine();
+        switch(day) {
+            case 1:
+                System.out.println("Domingo");
+                break;
+            case 2:
+                System.out.println("Segunda-feira");
+                break;
+            case 3:
+                System.out.println("Terça-feira");
+                break;
+            case 4:
+                System.out.println("Quarta-feira");
+                break;
+            case 5:
+                System.out.println("Quinta-feira");
+                break;
+            case 6:
+                System.out.println("Sexta-feira");
+                break;
+            case 7:
+                System.out.println("Sábado");
+                break;
+            default:
+                System.out.println("Escolha entre 1 e 7");
+        }
+    }
+}
