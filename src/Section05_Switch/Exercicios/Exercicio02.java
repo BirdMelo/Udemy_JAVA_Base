460ef957120d87a3a33969b68552f7529569e451
BirdMelo <jpmn2013.terra.com.br@gmail.com>
2025-11-08 11:03:34 -0300

    Exercicio02 de switch feito

---
+++ src/Section05_Switch/Exercicios/Exercicio02.java
@@ -0,0 +1,40 @@
+package Section05_Switch.Exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio02 {
+    public static void main(String[] args) {
+        Scanner write = new Scanner(System.in);
+        int num1, num2;
+        char operator;
+
+        System.out.print("Digite um número: ");
+        num1 = write.nextInt();
+        write.nextLine();
+        System.out.print("Digite outro número: ");
+        num2 = write.nextInt();
+        write.nextLine();
+        System.out.println("Qual operação deseja fazer?");
+        operator = write.nextLine().charAt(0);
+        switch (operator) {
+            case '+':
+                System.out.printf("%d %c %d = %d",
+                        num1, operator, num2, num1 + num2);
+                break;
+            case '-':
+                System.out.printf("%d %c %d = %d",
+                        num1, operator, num2, num1 - num2);
+                break;
+            case '*':
+                System.out.printf("%d %c %d = %d",
+                        num1, operator, num2, num1 * num2);
+                break;
+            case '/':
+                System.out.printf("%d %c %d = %.1f",
+                        num1, operator, num2, (double) num1 / num2);
+                break;
+            default:
+                System.out.println("Operador escolhido errado");
+        }
+    }
+}
