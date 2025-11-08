e38624b5e610763725ac831fe72b31cd85a7f6bc
BirdMelo <jpmn2013.terra.com.br@gmail.com>
2025-11-08 11:53:49 -0300

    Exercicio02 de while, feito

---
+++ src/Section06_While/Exercicios/Exemplo02.java
@@ -0,0 +1,11 @@
+package Section06_While.Exercicios;
+
+public class Exemplo02 {
+    public static void main(String[] args) {
+        int num = 0;
+        while (num <= 50) {
+            System.out.printf("Contador: %d\n", num);
+            num+=2;
+        }
+    }
+}
