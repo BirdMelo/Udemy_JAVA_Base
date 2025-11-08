b21fe3211a3965e61f5a2cef775897eb9525529f
BirdMelo <jpmn2013.terra.com.br@gmail.com>
2025-11-08 11:49:22 -0300

    Exercicio01 de while, feito

---
+++ src/Section06_While/Exercicios/Exercicio01.java
@@ -0,0 +1,11 @@
+package Section06_While.Exercicios;
+
+public class Exercicio01 {
+    public static void main(String[] args) {
+        int num = 1;
+        while (num <= 100) {
+            System.out.printf("Contagem: %d\n", num);
+            num++;
+        }
+    }
+}
